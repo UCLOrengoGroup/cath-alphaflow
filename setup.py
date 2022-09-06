780d768bb2e432552e00bdefded211120f541cee
Ian Sillitoe <ian@sillit.com>
2022-09-06 18:55:39 +0100

    add cx_Oracle

--- setup.py
+++ setup.py
@@ -31,10 +31,7 @@ setup(
         [console_scripts]
         cath-af-cli=cath_alphaflow.cli:cli
     """,
-    install_requires=[
-        "click",
-        "google-cloud-storage",
-    ],
+    install_requires=["click", "google-cloud-storage", "cx_oracle"],
     extras_require={"test": ["pytest"]},
     python_requires=">=3.7",
 )
