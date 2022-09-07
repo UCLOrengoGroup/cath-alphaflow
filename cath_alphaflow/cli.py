5de54939ea3937a555e30bf01987f7750c8ce36e
Ian Sillitoe <ian@sillit.com>
2022-09-07 18:47:26 +0100

    sort verbosity

--- cath_alphaflow/cli.py
+++ cath_alphaflow/cli.py
@@ -1,13 +1,28 @@
+import logging
 import click
 
 from .create_dataset import commands as create_dataset
 
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s | %(levelname)s | %(message)s"
+)
+
+LOG = logging.getLogger(__name__)
+
 
 @click.group()
 @click.version_option()
-def cli():
+@click.option("--verbose", "-v", "verbosity", default=0, count=True)
+@click.pass_context
+def cli(ctx, verbosity):
     "Workflow tools to assign CATH structural domains to AlphaFold predictions"
-    pass
+
+    root_logger = logging.getLogger()
+    log_level = root_logger.getEffectiveLevel() - (10 * verbosity)
+    root_logger.setLevel(log_level)
+    LOG.info(
+        f"Starting logging... (level={logging.getLevelName(root_logger.getEffectiveLevel())})"
+    )
 
 
 cli.add_command(create_dataset.create_dataset_uniprot_ids)
