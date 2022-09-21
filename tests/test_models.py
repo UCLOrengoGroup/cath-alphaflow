c138c97f34c2b6fa055145a84b24b0a21265d991
Ian Sillitoe <ian@sillit.com>
2022-09-21 19:03:16 +0100

    add tests

---
+++ tests/test_models.py
@@ -0,0 +1,56 @@
+import pytest
+
+from cath_alphaflow.errors import ParseError
+from cath_alphaflow.models import Segment, Chopping, AFChainID, AFDomainID
+
+
+def test_chopping_parser():
+
+    assert Chopping.from_str("123-456") == Chopping(
+        segments=[Segment(start=123, end=456)]
+    )
+
+    assert Chopping.from_str("123-456_789-1230") == Chopping(
+        segments=[Segment(start=123, end=456), Segment(start=789, end=1230)],
+    )
+
+
+def test_af_chain_parser():
+
+    assert AFChainID.from_str("AF-P00520-F1-v3") == AFChainID(
+        uniprot_acc="P00520", fragment_number=1, version=3
+    )
+
+    bad_ids = [
+        "P00520-F1-v3",
+        "AF_P00520_F1_v3",
+        "AF-P00520_-F1-v",
+        "AF-P00520-A-v3",
+        "AF-P00520-F1-v",
+    ]
+
+    for bad_id in bad_ids:
+        with pytest.raises(ParseError):
+            AFChainID.from_str(bad_id)
+
+
+def test_af_domain_parser():
+
+    assert AFDomainID.from_str("AF-P00520-F1-v3/12-234") == AFDomainID(
+        uniprot_acc="P00520",
+        fragment_number=1,
+        version=3,
+        chopping=Chopping(segments=[Segment(start=12, end=234)]),
+    )
+
+    bad_ids = [
+        "AF-P00520-F1-v3/12_234",
+        "AF-P00520-F1-v3/12-234_234",
+        "AF-P00520-F1-v3/-12-234",
+        "AF-P00520-F1-v3/12A-234",
+        "AF-P00520-F1-v3/12A-234",
+    ]
+
+    for bad_id in bad_ids:
+        with pytest.raises(ParseError):
+            AFDomainID.from_str(bad_id)
