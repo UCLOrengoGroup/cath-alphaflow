1b3f50faedf191f87f7180b8a138c5270ce43dda
Ian Sillitoe <ian@sillit.com>
2022-09-23 16:48:38 +0100

    tidy production settings

--- cath_alphaflow/settings.py
+++ cath_alphaflow/settings.py
@@ -2,15 +2,19 @@ from prettyconf import config
 
 
 class Settings:
-    ORACLE_DB_HOST = config("ORACLE_DB_HOST", default="Set ORACLE_DB_HOST")
-    ORACLE_DB_PORT = config("ORACLE_DB_PORT", default=1521)
-    ORACLE_DB_SID = config("ORACLE_DB_SID", default="Set ORACLE_DB_SID")
-    ORACLE_DB_USERNAME = config("ORACLE_DB_USERNAME", default="Set ORACLE_DB_USERNAME")
-    ORACLE_DB_PASSWORD = config("ORACLE_DB_PASSWORD", default="Set ORACLE_DB_PASSWORD")
+    ORACLE_DB_HOST = None
+    ORACLE_DB_PORT = None
+    ORACLE_DB_SID = None
+    ORACLE_DB_USERNAME = None
+    ORACLE_DB_PASSWORD = None
 
 
 class ProductionSettings(Settings):
-    pass
+    ORACLE_DB_HOST = config("ORACLE_DB_HOST")
+    ORACLE_DB_PORT = config("ORACLE_DB_PORT", default=1521)
+    ORACLE_DB_SID = config("ORACLE_DB_SID")
+    ORACLE_DB_USERNAME = config("ORACLE_DB_USERNAME")
+    ORACLE_DB_PASSWORD = config("ORACLE_DB_PASSWORD")
 
 
 class TestSettings(Settings):
