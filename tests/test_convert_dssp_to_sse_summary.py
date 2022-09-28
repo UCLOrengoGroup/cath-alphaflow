43ddd5bc0facd02a036597932300c27f3e2f163b
Ian Sillitoe <ian@sillit.com>
2022-09-28 16:57:18 +0100

    fix tests

--- tests/test_convert_dssp_to_sse_summary.py
+++ tests/test_convert_dssp_to_sse_summary.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 import csv
 from click.testing import CliRunner
@@ -5,6 +6,8 @@ from cath_alphaflow.cli import cli
 
 
 UNIPROT_IDS = ["P00520"]
+FIXTURE_PATH = Path(__file__).parent / "fixtures"
+EXAMPLE_DSSP_FILE = FIXTURE_PATH / "dssp" / "AF-P00520-F1-model_v3.dssp"
 
 SUBCOMMAND = "convert-dssp-to-sse-summary"
 
@@ -18,18 +21,19 @@ def test_cli_usage():
 
 
 def write_ids_to_file(fh, headers, ids):
-    fh.write("\t".join([h for h in headers]) + "\n")
+    writer = csv.writer(fh, delimiter="\t")
+    writer.writerow(headers)
     for _id in ids:
-        fh.write(f"{_id}\n")
+        writer.writerow([_id])
     fh.flush()
 
 
-def create_fake_dssp_dir(dirname, ids):
+def create_fake_dssp_dir(dirname, ids, dssp_src=EXAMPLE_DSSP_FILE):
     dir_path = Path(dirname)
     dir_path.mkdir()
     for _id in ids:
-        dssp_path = dir_path / f"{_id}.dssp"
-        dssp_path.open("wt")
+        dssp_path_dest = dir_path / f"{_id}.dssp"
+        os.symlink(dssp_src, f"{dssp_path_dest}")
     return dir_path
 
 
@@ -37,6 +41,12 @@ def test_convert_dssp(tmp_path):
 
     headers = ["header"]
     ids = ["id1", "id2"]
+
+    # TODO: fix the last two cols
+    expected_rows = [
+        ["313", "1123", "72.12822796081923", "???", "???"],
+        ["313", "1123", "72.12822796081923", "???", "???"],
+    ]
 
     runner = CliRunner()
     with runner.isolated_filesystem(temp_dir=tmp_path):
@@ -52,14 +62,12 @@ def test_convert_dssp(tmp_path):
         args = (
             SUBCOMMAND,
             "--dssp_dir",
-            tmp_dssp_path.name,
+            f"{tmp_dssp_path}",
             "--id_file",
-            tmp_id_path.name,
+            f"{tmp_id_path}",
             "--sse_out_file",
-            tmp_sse_path.name,
+            f"{tmp_sse_path}",
         )
-        print(f"cd {Path.cwd()}")
-        print(f"cath-af-cli {' '.join(args)}")
         result = runner.invoke(cli, args)
         assert result.exit_code == 0
         assert "DONE" in result.output
@@ -68,16 +76,16 @@ def test_convert_dssp(tmp_path):
         assert expected_outfile_path.exists()
 
         with expected_outfile_path.open("rt") as fh:
-            csvreader = csv.reader(fh)
+            csvreader = csv.reader(fh, delimiter="\t")
             got_headers = next(csvreader)
             got_rows = list(csvreader)
 
-        assert got_headers == headers
-        assert len(got_rows) == len(rows)
-        assert got_rows == [
+        assert got_headers == [
             "ss_res_total",
             "res_count",
             "perc_not_in_ss",
             "sse_H_num",
             "sse_E_num",
         ]
+        assert len(got_rows) == len(ids)
+        assert got_rows == expected_rows
