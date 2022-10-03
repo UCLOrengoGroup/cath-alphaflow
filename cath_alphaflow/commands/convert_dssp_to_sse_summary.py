85326e55dfb3c7b6e212181ebf453265d8ac7b1a
Nicola Bordin <bordin89@gmail.com>
2022-10-03 20:29:27 +0200

    refactor summary calc from dssp

--- cath_alphaflow/commands/convert_dssp_to_sse_summary.py
+++ cath_alphaflow/commands/convert_dssp_to_sse_summary.py
@@ -49,17 +49,21 @@ def convert_dssp_to_sse_summary(dssp_dir, id_file, sse_out_file, dssp_suffix):
         click.echo(file_stub)
         dssp_path = Path(dssp_dir) / f"{file_stub}{dssp_suffix}"
         ss_sum = get_sse_summary_from_dssp(dssp_path)
-        sse_out_writer.writerow(ss_sum.__dict__)
+        sse_out_writer.writerow(ss_sum.to_dict())
 
     click.echo("DONE")
 
 
-def get_sse_summary_from_dssp(dssp_path, *, chopping=None) -> SecStrSummary:
+def get_sse_summary_from_dssp(
+    dssp_path: Path, *, chopping=None, acc_id=None
+) -> SecStrSummary:
 
     dssp_string = []
     read_headers = False
     domain_length = 0
     ss_total = 0
+    if acc_id is None:
+        acc_id = dssp_path.stem
 
     with dssp_path.open("rt") as dssp_fh:
         for line in dssp_fh:
@@ -77,47 +81,4 @@ def get_sse_summary_from_dssp(dssp_path, *, chopping=None) -> SecStrSummary:
     else:
         segment_dssp = dssp_string
 
-    # Calculate percentage of residues in secondary structures
-    ss_total += segment_dssp.count("H") + segment_dssp.count("E")
-    domain_length += len(segment_dssp)
-    if domain_length == 0:
-        msg = f"failed to find any SS data in DSSP file '{dssp_path}'"
-        raise ParseError(msg)
-    else:
-        perc_not_in_ss = round(((domain_length - ss_total) / domain_length) * 100, 2)
-
-    # Calculate number of SSEs
-    sse_H_num = sse_H_res = sse_E_num = sse_E_res = 0
-    sse_H = sse_E = False
-
-    # Calculate number of alpha helices
-    for residue in dssp_string:
-        if residue == "H":
-            sse_H_res += 1
-            if sse_H_res > DEFAULT_HELIX_MIN_LENGTH and sse_H == False:
-                sse_H = True
-                sse_H_num += 1
-
-        if residue == "E":
-            sse_E_res += 1
-            if sse_E_res > DEFAULT_STRAND_MIN_LENGTH and sse_E == False:
-                sse_E = True
-                sse_E_num += 1
-
-        if residue != "H" and residue != "E":
-            sse_H = sse_E = False
-            sse_H_res = sse_E_res = 0
-
-    sse_num = sse_H_num + sse_E_num
-
-    ss_sum = SecStrSummary(
-        af_domain_id=dssp_path,
-        ss_res_total=ss_total,
-        res_count=domain_length,
-        perc_not_in_ss=perc_not_in_ss,
-        sse_H_num=sse_H_num,
-        sse_E_num=sse_E_num,
-        sse_num=sse_num,
-    )
-
-    return ss_sum
+    return SecStrSummary.new_from_dssp_str(segment_dssp, acc_id)
