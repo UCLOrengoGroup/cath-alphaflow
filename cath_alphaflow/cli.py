1f8e46bdb6b5722c947b50150bfa3c15545737f2
Nicola Bordin <bordin89@gmail.com>
2022-10-17 14:33:49 +0100

    Add CLI option for convert_cif_to_foldseek_db

--- cath_alphaflow/cli.py
+++ cath_alphaflow/cli.py
@@ -8,6 +8,7 @@ from .commands import optimise_domain_boundaries
 from .commands import convert_dssp_to_sse_summary
 from .commands import convert_cif_to_dssp
 from .commands import extract_plddt_and_lur
+from .commands import convert_cif_to_foldseek_db
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s | %(levelname)s | %(message)s"
@@ -49,3 +50,4 @@ cli.add_command(optimise_domain_boundaries.optimise_domain_boundaries)
 cli.add_command(convert_dssp_to_sse_summary.convert_dssp_to_sse_summary)
 cli.add_command(convert_cif_to_dssp.convert_cif_to_dssp)
 cli.add_command(extract_plddt_and_lur.convert_cif_to_plddt_summary)
+cli.add_command(convert_cif_to_foldseek_db.convert_cif_to_foldseek_db)
