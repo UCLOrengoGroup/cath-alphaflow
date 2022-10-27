8dbc269b0d230a5d16424a1d45bab7336110a323
Nicola Bordin <bordin89@gmail.com>
2022-10-27 14:59:53 +0100

    Include filters for overlap and bits Use iterators

--- cath_alphaflow/commands/convert_foldseek_output_to_summary.py
+++ cath_alphaflow/commands/convert_foldseek_output_to_summary.py
@@ -1,6 +1,11 @@
 import logging
+from unittest import result
 import click
-from cath_alphaflow.io_utils import yield_first_col, get_foldseek_summary_writer
+from cath_alphaflow.io_utils import (
+    yield_first_col,
+    get_foldseek_reader,
+    get_foldseek_summary_writer,
+)
 from cath_alphaflow.settings import get_default_settings
 from cath_alphaflow.constants import DEFAULT_FS_BITS_CUTOFF, DEFAULT_FS_OVERLAP
 from cath_alphaflow.models import FoldseekSummary
@@ -19,7 +24,7 @@ LOG = logging.getLogger()
 )
 @click.option(
     "--fs_input_file",
-    type=click.Path(exists=True, file_okay=True, resolve_path=True),
+    type=click.File("rt"),
     default="fs_query_results.m8",
     help=f"Foldseek tabular output as input",
 )
@@ -30,78 +35,22 @@ LOG = logging.getLogger()
     help=f"Foldseek hits file",
 )
 def convert_foldseek_output_to_summary(id_file, fs_input_file, fs_results):
-    fs_results_writer = get_foldseek_summary_writer(fs_results)
-    best_hits_dict = extract_best_hits_foldseek(fs_input_file)
+    unique_af_ids = set()
+    unique_af_ids.add("NOHIT")
+    best_hits = set()
+    foldseek_results_writer = get_foldseek_summary_writer(fs_results)
     for file_stub in yield_first_col(id_file):
-        extract_hits_from_hits_dict_foldseek(file_stub, best_hits_dict)
-
-
-def extract_best_hits_foldseek(fs_input_file):
-    "Convert Foldseek tabular output to summary of best hits"
-    results_dict = {}
-    with open(fs_input_file, "rt") as fs_fh:
-        for line in fs_fh:
-            line = line.rstrip()
-            (
-                query,
-                target,
-                qstart,
-                qend,
-                qlen,
-                tstart,
-                tend,
-                tlen,
-                qcov,
-                tcov,
-                bits,
-                evalue,
-            ) = line.split()
-            if query.endswith(".cif"):
-                query = query[:-4]
-            if query in results_dict:
-                continue
-            results_dict[query] = [
-                target,
-                qstart,
-                qend,
-                qlen,
-                tstart,
-                tend,
-                tlen,
-                qcov,
-                tcov,
-                bits,
-                evalue,
-            ]
-    return results_dict
-
-
-def extract_hits_from_hits_dict_foldseek(query_id, best_hits_dict):
-    no_hits_placeholder = ""
-    if query_id in best_hits_dict:
-        (
-            target,
-            qstart,
-            qend,
-            qlen,
-            tstart,
-            tend,
-            tlen,
-            qcov,
-            tcov,
-            bits,
-            evalue,
-        ) = best_hits_dict[query_id]
-    return FoldseekSummary(
-        target,
-        qstart,
-        qend,
-        qlen,
-        tstart,
-        tend,
-        tlen,
-        qcov,
-        tcov,
-        bits,
-        evalue=no_hits_placeholder,
-    )
+        unique_af_ids.add(file_stub)
+    foldseek_reader = get_foldseek_reader(fs_input_file)
+    for foldseek_result_as_dict in foldseek_reader:
+        result = FoldseekSummary(**foldseek_result_as_dict)
+        if result.query.endswith(".cif"):
+            result.query = result.query[:-4]
+        if (
+            result.query not in best_hits
+            and float(result.tcov) >= DEFAULT_FS_OVERLAP
+            and int(result.bits) >= DEFAULT_FS_BITS_CUTOFF
+            and result.query in unique_af_ids
+        ):
+            best_hits.add(result.query)
+            foldseek_results_writer.writerow(result.__dict__)
