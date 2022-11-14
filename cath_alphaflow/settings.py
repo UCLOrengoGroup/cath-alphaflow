d18955ec81f935a01e52621dc35f194a337a86c7
Ian Sillitoe <ian@sillit.com>
2022-11-14 16:49:21 +0000

    provide defaults for foldseek settings

--- cath_alphaflow/settings.py
+++ cath_alphaflow/settings.py
@@ -1,8 +1,15 @@
-from email.policy import default
+from pathlib import Path
 from prettyconf import config
 
 DEFAULT_AF_VERSION = 3
 DEFAULT_AF_FRAGMENT = 1
+
+PROJECT_ROOT_DIR = Path(__file__).parent.parent
+DEFAULT_FS_BINARY_PATH = str(PROJECT_ROOT_DIR / "foldseek" / "bin" / "foldseek")
+
+
+def resolve_path(raw_path_str):
+    return str(Path(raw_path_str).resolve())
 
 
 class Settings:
@@ -13,9 +20,13 @@ class Settings:
     ORACLE_DB_PASSWORD = config("ORACLE_DB_PASSWORD", default=None)
     DSSP_BINARY_PATH = config("DSSP_BINARY_PATH", default=None)
     DSSP_PDB_DICT = config("DSSP_PDB_DICT", default=None)
-    FS_BINARY_PATH = config("FS_BINARY_PATH", default=None)
-    FS_DB_PATH = config("FS_DB_PATH", default=None)
-    FS_TMP_PATH = config("FS_TMP_PATH", default=None)
+    FS_BINARY_PATH = config(
+        "FS_BINARY_PATH",
+        default=DEFAULT_FS_BINARY_PATH,
+        cast=resolve_path,
+    )
+    FS_DB_PATH = config("FS_DB_PATH", default="foldseek_db", cast=resolve_path)
+    FS_TMP_PATH = config("FS_TMP_PATH", default="foldseek_tmp", cast=resolve_path)
 
     def to_dict(self):
         dict = {}
