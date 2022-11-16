1ed99dd70f31bbf7510c097ba1cfaf25aada561a
Ian Sillitoe <ian@sillit.com>
2022-11-16 18:55:28 +0000

    implement policy on file errors

--- cath_alphaflow/commands/chop_domain.py
+++ cath_alphaflow/commands/chop_domain.py
@@ -11,6 +11,12 @@ from cath_alphaflow.chopping import chop_cif
 
 LOG = logging.getLogger()
 
+FILE_POLICY_SKIP = "skip"
+FILE_POLICY_ERROR = "error"
+FILE_POLICY_OVERWRITE = "overwrite"
+DEFAULT_INPUT_FILE_POLICY = FILE_POLICY_SKIP
+DEFAULT_OUTPUT_FILE_POLICY = FILE_POLICY_SKIP
+
 ID_TYPE_AF_DOMAIN = "af"
 ID_TYPE_UNIPROT_DOMAIN = "uniprot"
 DEFAULT_AF_FRAGMENT_NUMBER = 1
@@ -43,6 +49,29 @@ VALID_CIF_SUFFIXES = [".cif", ".cif.gz"]
     help=f"Option: specify the type of ID to specify the chopping [{ID_TYPE_AF_DOMAIN}]",
 )
 @click.option(
+    "--input_file_policy",
+    type=click.Choice(
+        [
+            FILE_POLICY_ERROR,
+            FILE_POLICY_SKIP,
+        ]
+    ),
+    default=DEFAULT_INPUT_FILE_POLICY,
+    help=f"Option: specify behaviour on missing input file [{DEFAULT_INPUT_FILE_POLICY}]",
+)
+@click.option(
+    "--output_file_policy",
+    type=click.Choice(
+        [
+            FILE_POLICY_OVERWRITE,
+            FILE_POLICY_ERROR,
+            FILE_POLICY_SKIP,
+        ]
+    ),
+    default=DEFAULT_OUTPUT_FILE_POLICY,
+    help=f"Option: specify behaviour on encountering an existing output file [{DEFAULT_OUTPUT_FILE_POLICY}]",
+)
+@click.option(
     "--af_version",
     type=int,
     help=f"Option: specify the AF version when parsing uniprot ids",
@@ -52,6 +81,8 @@ def chop_cif_command(
     id_file,
     id_type,
     cif_out_dir,
+    input_file_policy,
+    output_file_policy,
     af_version,
 ):
     "Apply chopping to CIF files"
@@ -81,14 +112,29 @@ def chop_cif_command(
 
         if not chain_cif_path.exists():
             msg = f"failed to locate CIF input file {cif_in_dir}/{af_chain_stub}{VALID_CIF_SUFFIXES}"
-            LOG.error(msg)
-            raise FileNotFoundError(msg)
+            if input_file_policy == FILE_POLICY_SKIP:
+                LOG.warning(msg + " (skipping)")
+            elif input_file_policy == FILE_POLICY_ERROR:
+                LOG.error(msg)
+                raise FileNotFoundError(msg)
+            else:
+                msg = f"unexpected input file policy {input_file_policy}"
+                click.UsageError(msg)
 
         domain_cif_path = Path(cif_out_dir) / (af_domain_id.to_file_stub() + cif_suffix)
         if domain_cif_path.exists():
             msg = f"output domain CIF file already exists: {domain_cif_path}"
-            LOG.error(msg)
-            raise FileExistsError(msg)
+            if output_file_policy == FILE_POLICY_OVERWRITE:
+                LOG.warning(msg + " (overwriting)")
+            elif output_file_policy == FILE_POLICY_ERROR:
+                LOG.error(msg)
+                raise FileExistsError(msg)
+            elif output_file_policy == FILE_POLICY_SKIP:
+                LOG.info(msg + " (skipping)")
+                continue
+            else:
+                msg = f"unexpected output file policy {output_file_policy}"
+                raise click.UsageError(msg)
 
         chop_cif(
             domain_id=af_domain_id.to_str(),
