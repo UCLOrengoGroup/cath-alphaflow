1101c6645063cbec9f3b5408f3458855f65782ae
Rachel-Alcraft <r.alcraft@ucl.ac.uk>
2023-02-02 15:59:16 +0000

    Scripts for different scenarios

---
+++ platforms/ucl_myriad/next_localnodes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -l
+
+SCRIPT_DIR=~/repos/cath-alphaflow/platforms/ucl_myriad
+source ${SCRIPT_DIR}/include
+module load python3
+module load java/openjdk-11/11.0.1
+qsub ./nextflow run workflows/cath-test-workflow.nf -c platforms/ucl_myriad/nextflow.config
