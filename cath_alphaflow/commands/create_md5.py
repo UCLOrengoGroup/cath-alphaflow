077977f0086c35c1ab512b5845d7bec8823d52b8
Nicola Bordin <bordin89@gmail.com>
2023-02-28 15:31:09 +0000

    Tidy create_md5 Create test create_md5 Add fixtures for create_md5 test

--- cath_alphaflow/commands/create_md5.py
+++ cath_alphaflow/commands/create_md5.py
@@ -5,12 +5,6 @@ from Bio import SeqIO
 
 from cath_alphaflow.io_utils import get_af_uniprot_md5_summary_writer
 from cath_alphaflow.seq_utils import str_to_md5
-from cath_alphaflow.models import AFChainID
-from cath_alphaflow.constants import (
-    ID_TYPE_AF_DOMAIN,
-    ID_TYPE_UNIPROT_DOMAIN,
-    ID_TYPE_SIMPLE,
-)
 from cath_alphaflow.errors import ParseError
 
 DEFAULT_CHUNK_SIZE = 1000000
@@ -38,22 +32,19 @@ def create_md5(fasta_file, uniprot_md5_csv_file):
     with uniprot_md5_csv_file as out_fh:
         md5_out_writer = get_af_uniprot_md5_summary_writer(out_fh)
         # work through fasta file, calculate output for relevant records
-        with fasta_file as fasta_fh:
-            for record in SeqIO.parse(fasta_fh, "fasta"):
-                click.echo(f"record: {record.id} seq='{record.seq[:10]}...'")
-                # >AFDB:AF-A0A2L2JPH6-F1
-                try:
-                    af_chain_id = record.id.split(":")[1]
-                    af_uniprot_id = af_chain_id.split("-")[1]
-                except:
-                    raise ParseError(
-                        f"Failed to parse {record.id} to AlphaFold chain ID"
-                    )
-                row_data = {
-                    "af_chain_id": af_chain_id,
-                    "uniprot_id": af_uniprot_id,
-                    "sequence_md5": str_to_md5(str(record.seq)),
-                }
-                md5_out_writer.writerow(row_data)
+        for record in SeqIO.parse(fasta_file, "fasta"):
+            click.echo(f"record: {record.id} seq='{record.seq[:10]}...'")
+            # >AFDB:AF-A0A2L2JPH6-F1
+            try:
+                af_chain_id = record.id.split(":")[1]
+                af_uniprot_id = af_chain_id.split("-")[1]
+            except:
+                raise ParseError(f"Failed to parse {record.id} to AlphaFold chain ID")
+            row_data = {
+                "af_chain_id": af_chain_id,
+                "uniprot_id": af_uniprot_id,
+                "sequence_md5": str_to_md5(str(record.seq)),
+            }
+            md5_out_writer.writerow(row_data)
 
     click.echo("DONE")
