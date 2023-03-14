53dd10c31230bc85f61e1606f59d4038f0aa44d2
Nicola Bordin <bordin89@gmail.com>
2023-03-14 16:24:44 +0000

    Switch from file_stub to id_type based reader Add comments Introduce filters

--- cath_alphaflow/commands/convert_foldseek_output_to_summary.py
+++ cath_alphaflow/commands/convert_foldseek_output_to_summary.py
@@ -6,9 +6,10 @@ from cath_alphaflow.io_utils import (
     get_foldseek_reader,
     get_foldseek_summary_writer,
 )
-from cath_alphaflow.settings import get_default_settings
-from cath_alphaflow.constants import DEFAULT_FS_BITS_CUTOFF, DEFAULT_FS_OVERLAP
-from cath_alphaflow.models.domains import FoldseekSummary
+from cath_alphaflow.settings import get_default_settings, DEFAULT_AF_VERSION
+from cath_alphaflow.constants import DEFAULT_FS_BITS_CUTOFF, DEFAULT_FS_OVERLAP,ID_TYPE_AF_DOMAIN,ID_TYPE_UNIPROT_DOMAIN
+from cath_alphaflow.models.domains import FoldseekSummary,AFDomainID
+from cath_alphaflow.errors import ArgumentError
 
 config = get_default_settings()
 
@@ -20,37 +21,87 @@ LOG = logging.getLogger()
     "--id_file",
     type=click.File("rt"),
     required=True,
-    help="Input: CSV file containing list of ids to convert from CIF to DSSP",
+    help="Input: CSV file containing list of ids to process.",
+)
+@click.option(
+    "--id_type",
+    type=click.Choice([ID_TYPE_AF_DOMAIN, ID_TYPE_UNIPROT_DOMAIN]),
+    default=ID_TYPE_AF_DOMAIN,
+    help=f"Option: specify the type of ID in id_file [{ID_TYPE_AF_DOMAIN}]",
 )
 @click.option(
     "--fs_input_file",
     type=click.File("rt"),
-    default="fs_query_results.m8",
+    required=True,
     help=f"Foldseek tabular output as input",
 )
 @click.option(
     "--fs_results",
     type=click.File("wt"),
-    default="fs_hits.tsv",
-    help=f"Foldseek hits file",
+    required=True,
+    help=f"Foldseek results summary file",
 )
-def convert_foldseek_output_to_summary(id_file, fs_input_file, fs_results):
+@click.option(
+    "--af_version",
+    type=int,
+    default=DEFAULT_AF_VERSION,
+    help=f"Option: specify the AF version when parsing uniprot ids. (default:{DEFAULT_AF_VERSION})",
+)
+
+def convert_foldseek_output_to_summary(id_file, fs_input_file, fs_results, id_type, af_version):
     unique_af_ids = set()
     unique_af_ids.add("NOHIT")
-    best_hits = set()
+    best_hit_by_query = {}
     foldseek_results_writer = get_foldseek_summary_writer(fs_results)
-    for file_stub in yield_first_col(id_file):
-        unique_af_ids.add(file_stub)
+    # Build set of unique AF IDs
+    for af_domain_id_str in yield_first_col(id_file):
+        if id_type == ID_TYPE_UNIPROT_DOMAIN:
+            af_domain_id = AFDomainID.from_uniprot_str(
+                af_domain_id_str,version=af_version
+            )
+            af_domain_id_str = af_domain_id.to_file_stub()
+        elif id_type == ID_TYPE_AF_DOMAIN:
+            af_domain_id = AFDomainID.from_str(af_domain_id_str)
+            af_domain_id_str = af_domain_id.to_file_stub()
+        else:
+            msg = f"failed to understand id_type '${id_type}'"
+            raise ArgumentError(msg)
+        unique_af_ids.add(af_domain_id_str)
+   
+    # Build Foldseek Reader
     foldseek_reader = get_foldseek_reader(fs_input_file)
+
+    # Extract best hit per query for filtered ids
     for foldseek_result_as_dict in foldseek_reader:
         result = FoldseekSummary(**foldseek_result_as_dict)
-        if result.query.endswith(".cif"):
-            result.query = result.query[:-4]
-        if (
-            result.query not in best_hits
-            and float(result.tcov) >= DEFAULT_FS_OVERLAP
-            and int(result.bits) >= DEFAULT_FS_BITS_CUTOFF
-            and result.query in unique_af_ids
-        ):
-            best_hits.add(result.query)
-            foldseek_results_writer.writerow(result.__dict__)
+        
+        """
+        result: 
+        {'query': 'AF-A0A059CHW2-F1-model_v4-22-322.cif', 'target': '1xhlA00', 'qstart': '7', 'qend': '271', 
+         'qlen': '301', 'tstart': '3', 'tend': '252', 'tlen': '274', 'qcov': '0.880', 'tcov': '0.912', 
+         'bits': '509', 'evalue': '1.305E-11'}
+        """
+        
+        af_query_id = AFDomainID.from_foldseek_query(result.query)
+        
+        
+        af_query_id_str = af_query_id.from_str(str(af_query_id)).to_file_stub()
+    
+        if af_query_id_str not in unique_af_ids:
+            continue
+        
+        if float(result.tcov) <= DEFAULT_FS_OVERLAP or int(result.bits) <= DEFAULT_FS_BITS_CUTOFF:
+            continue
+
+        if af_query_id_str not in best_hit_by_query:
+            best_hit_by_query[af_query_id_str] = result
+
+        if int(result.bits) > int(best_hit_by_query[af_query_id_str].bits):
+            best_hit_by_query[af_query_id_str] = result
+        
+
+
+        
+    # Write results to summary file
+    for af_query_id, result in best_hit_by_query.items():   
+        foldseek_results_writer.writerow(result.__dict__)
