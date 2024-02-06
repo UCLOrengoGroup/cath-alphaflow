5bd53e939a8a8657a6dd0d7ababc16ca7ee6587a
Ian Sillitoe <ian@sillit.com>
2024-02-06 12:47:24 +0000

    add tests for PDB chopping

--- tests/test_chopping.py
+++ tests/test_chopping.py
@@ -6,12 +6,16 @@ import tempfile
 
 from Bio import SeqIO
 from Bio.PDB import MMCIFParser
+from Bio.PDB import PDBParser
 from Bio.PDB.Polypeptide import protein_letters_3to1
 
 from cath_alphaflow.models.domains import Chopping, AFDomainID
-from cath_alphaflow.chopping import ChoppingProcessor, chop_cif
+from cath_alphaflow.chopping import ChoppingProcessor
+from cath_alphaflow.chopping import chop_cif
+from cath_alphaflow.chopping import chop_structure
 
 FIXTURE_PATH = Path(__file__).parent / "fixtures"
+PDB_DIR = FIXTURE_PATH / "pdb"
 EXAMPLE_CIF_FILE = FIXTURE_PATH / "cif" / "AF-P00520-F1-model_v3.cif.gz"
 
 MULTI_FRAG_EXAMPLE_CIF_PATH = FIXTURE_PATH / "cif" / "AF-Q15772-F11-model_v4.cif.gz"
@@ -28,6 +32,15 @@ def example_cif_gzipped_path():
 @pytest.fixture
 def example_cif_tmpfile(example_cif_gzipped_path):
     return get_unzipped_cif_file(example_cif_gzipped_path)
+
+
+def get_tmp_pdb_file(pdb_path):
+    tmp_pdb_file = tempfile.NamedTemporaryFile(mode="wt", suffix=".pdb")
+    with open(str(pdb_path), mode="rt") as fh:
+        for line in fh:
+            tmp_pdb_file.write(line)
+    tmp_pdb_file.flush()
+    return tmp_pdb_file
 
 
 def get_unzipped_cif_file(zipped_cif_path):
@@ -104,6 +117,69 @@ def test_chop_cif(example_cif_tmpfile, example_chopping_str, example_expected_re
     resids = [res.get_id() for res in list(structure.get_residues())]
 
     assert resids == example_expected_resids
+
+
+def test_chop_pdb():
+    example_pdb_file = PDB_DIR / "5yh0J.pdb"
+    example_pdb_tmpfile = get_tmp_pdb_file(example_pdb_file)
+    example_chopping_str = "135-366"
+    # expected_atom_labels = "135-159,198-366"
+    example_expected_resids = [
+        (" ", i, " ") for i in list(range(135, 159 + 1)) + list(range(198, 366 + 1))
+    ]
+    new_pdb_tmpfile = tempfile.NamedTemporaryFile(mode="wt", suffix=".pdb")
+    example_chopping = Chopping.from_str(example_chopping_str)
+
+    chop_structure(
+        domain_id="1abc",
+        chain_path=Path(example_pdb_tmpfile.name),
+        domain_path=Path(new_pdb_tmpfile.name),
+        chopping=example_chopping,
+    )
+
+    assert Path(new_pdb_tmpfile.name).exists()
+
+    parser = PDBParser()
+    structure = parser.get_structure("1abc", new_pdb_tmpfile.name)
+    # this structure should only contain residues from the given chopping and nothing else
+    resids = [res.get_id() for res in list(structure.get_residues())]
+
+    assert resids == example_expected_resids
+
+
+def test_chopping_equals_filter_bio_residues():
+    example_pdb_file = PDB_DIR / "5yh0J.pdb"
+    example_pdb_tmpfile = get_tmp_pdb_file(example_pdb_file)
+    example_chopping_str = "135-366"
+    # expected_atom_labels = "135-159,198-366"
+    example_expected_resids = [
+        (" ", i, " ") for i in list(range(135, 159 + 1)) + list(range(198, 366 + 1))
+    ]
+    new_pdb_tmpfile = tempfile.NamedTemporaryFile(mode="wt", suffix=".pdb")
+    example_chopping = Chopping.from_str(example_chopping_str)
+
+    # get the residues by chopping the file
+    chop_structure(
+        domain_id="1abc",
+        chain_path=Path(example_pdb_tmpfile.name),
+        domain_path=Path(new_pdb_tmpfile.name),
+        chopping=example_chopping,
+    )
+
+    assert Path(new_pdb_tmpfile.name).exists()
+
+    # get the residues by filtering the Bio.Structure residues
+    parser = PDBParser()
+    structure = parser.get_structure("1abc", new_pdb_tmpfile.name)
+    filtered_residues = example_chopping.filter_bio_residues(structure.get_residues())
+
+    filtered_resids = [res.get_id() for res in list(filtered_residues)]
+
+    # this structure should only contain residues from the given chopping and nothing else
+    resids = [res.get_id() for res in list(structure.get_residues())]
+
+    assert resids == example_expected_resids
+    assert resids == filtered_resids
 
 
 def test_chop_multi_fragment():
